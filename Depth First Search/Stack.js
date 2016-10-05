b4d2b70ae6a6c8b254df83d5c99de76a26cf5a45
Timothy Hennessy <timothy.hennessy23@gmail.com>
2016-10-05 09:46:20 -0400

    Initial Upload
    Passed testing.

--- Depth First Search/Stack.js
+++ Depth First Search/Stack.js
@@ -1,21 +1,10 @@
-function makeNode(value) {
-	if (value === null || value === undefined) {
-		console.log("Error: cannot make node."); 
-		return;
-	}
-	return newNode = {
-		value : value,
-		next  : null
-	};
-}
-
-var stack = {
+var frontier = {
 	N : 0,
 	front : null,
 	isEmpty : function() {return this.N === 0;},
 	push : function(node) {
 		if (node === null || node === undefined) {
-			console.log("Error: attempt to push invalid ndoe."); 
+			console.log("Error: attempt to push invalid node."); 
 			return;
 		}
 		var oldFirst = this.front;
@@ -25,10 +14,11 @@ var stack = {
 	},
 	pop : function() {
 		if (!this.isEmpty()) {
-			var value = this.front.value; // Just save value
-			this.front = this.front.next; // Overwrite previous first node
+			var currentBoard = this.front.currBoard;      // Just save value
+			var previousBoard = this.front.prevBoard;
+			this.front = this.front.next;                 // Overwrite previous first node
 			this.N--;
-		 	return makeNode(value);       // Make new node with value, and next as null
+		 	return makeNode(currentBoard, previousBoard); // Make new node with value, and next as null
 		} else {
 			console.log("Error: stack underflow.");
 			process.exit(0);
